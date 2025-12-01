bd8031aac8967c5a563d348a32ade35fb142ed09
Shaden Saeed Alghamdi <shadenvillee@gmail.com>
2025-12-01 16:22:16 +0300

    Update-SEO final code.py

--- final code.py
+++ final code.py
@@ -15,10 +15,12 @@ from typing import Callable, Dict, Tuple, List
 # --------------------------------
 
 st.set_page_config(
-    page_title="الحاسبة المالية المتقدمة | فوائد وقروض", # <-- عنوان محسن
+    page_title="الحاسبة المالية المتقدمة | فوائد وقروض",
     page_icon="llogo.png",
     layout="centered",
-    initial_sidebar_state="collapsed"
+    initial_sidebar_state="collapsed", # Shaden Al-Ghamdi
+    # إضافة كود التحقق من جوجل هنا
+    meta_tags=[{"name": "google-site-verification", "content": "<meta name="google-site-verification" content="Dt6IB31gKNtjpGLr_gODgn8mCulRreKzrS7mXdTzJxU" />"}]
 )
 
 def load_css(file_name: str):
